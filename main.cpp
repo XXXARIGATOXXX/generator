157f1361917f90936deda973ae3edfee4443974f
XXXARIGATOXXX <128591382+XXXARIGATOXXX@users.noreply.github.com>
2023-04-06 20:57:18 +0300

    Generator updated

--- main.cpp
+++ main.cpp
@@ -4,6 +4,17 @@
 using namespace std;
 
 
+int Summa(int* counters, int size)
+{
+  int tmp = 0;
+  for (int i = 0; i < size ; i++)
+  {
+    tmp += counters[i];
+    
+  }
+  return tmp;
+}
+
 int GetRandomInt(int min, int max)
 {
     if ( min == max ) // сравнение вводимых значений
@@ -21,50 +32,51 @@ int GetRandomInt(int min, int max)
 
 
 int main() 
-{
+{   
+
     std::srand(std::time(nullptr)); // use current time as seed for random generator
+    int counters[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     
 
-    for(int i= 1; i <= 101;i++)
+    for(int i= 1; i <= 100;i++)
     {
       int number = GetRandomInt(0,9);
       cout << number << endl;
-
+      
       switch(number)
       {
         case 0:
-            int count0++;
+            counters[0]++;
             break;
         case 1:
-            int count1++;
+            counters[1]++;
             break;
         case 2:
-            int count2++;
+            counters[2]++;
             break;
         case 3:
-            int count3++;
+            counters[3]++;
             break;
         case 4:
-            int count4++;
+            counters[4]++;
             break;
         case 5:
-            int count5++;
+            counters[5]++;
             break;
         case 6:
-            int count6++;
+            counters[6]++;
             break;
         case 7:
-            int count7++;
+            counters[7]++;
             break;
         case 8:
-            int count8++;
+            counters[8]++;
             break;
         case 9:
-            int count9++;
+            counters[9]++;
             break;
        }
     }
-
-
-
+cout << Summa(counters, 10) << endl;
 }
+
